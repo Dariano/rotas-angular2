8b5ecd12e9b2e51f94fec23fe512b33db54e45cd
dariano <darianosoares@hotmail.com>
2017-03-01 11:02:42 -0300

    Modularizando as rodas.

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
 
+import { CursosRouterModule } from './cursos.routing.module';
+
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        CursosRouterModule
     ],
     exports: [],
     declarations: [
